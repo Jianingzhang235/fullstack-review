5b7285ed58eaafbd9ac605b937025fd68ef98162
Jianingzhang235 <jianing_chang@hotmail.com>
2019-05-11 23:50:21 -0700

    finished the ajax

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,17 +7,52 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
 
+    this.getRepos = this.getRepos.bind(this);
   }
 
   search (term) {
     console.log(`${term} was searched`);
+       $.ajax({
+         url: '',
+         method: 'POST',
+         data: term,
+         success: function (data) {
+           console.log(results, 'get req from client successfully')
+           //setState
+           this.setState({
+             repos: results
+           });
+         },
+         context: this,
+         error: function (results) {
+           console.log('error in Get req from client')
+         }
+       })
+
     // TODO
   }
+   getRepos () {
 
+      $.ajax({
+        url: '',
+        method: 'GET',
+        success: function(results) {
+          console.log(results, 'get req from client successfully')
+          //setState
+          this.setState({repos: results});
+        },
+        context: this,
+        error: function(results) {
+          console.log('error in Get req from client')
+        }
+      })
+    // send a post req to server to get top 25 repos form API, and saved in DB;
+    //
+  }
   render () {
     return (<div>
       <h1>Github Fetcher</h1>
@@ -26,5 +61,6 @@ class App extends React.Component {
     </div>)
   }
 }
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
